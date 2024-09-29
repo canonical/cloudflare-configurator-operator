eb9732f8448fd767926c461b46dd67de8eacb2d2
Weii Wang <weii.wang@canonical.com>
2024-09-29 18:30:08 +0000

    Initial charm

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -4,36 +4,63 @@
 # See LICENSE file for licensing details.
 
 """Integration tests."""
-
-import asyncio
+import json
 import logging
-from pathlib import Path
 
 import pytest
-import yaml
-from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
-METADATA = yaml.safe_load(Path("./metadata.yaml").read_text(encoding="utf-8"))
-APP_NAME = METADATA["name"]
-
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
-    """Deploy the charm together with related charms.
-
-    Assert on the unit status before any relations/configurations take place.
+async def test_build_and_deploy(
+    model, cloudflare_configurator, ingress_requirer, cloudflared_route_requirer
+):
     """
-    # Deploy the charm and wait for active/idle status
-    charm = pytestconfig.getoption("--charm-file")
-    resources = {"httpbin-image": METADATA["resources"]["httpbin-image"]["upstream-source"]}
-    assert ops_test.model
-    await asyncio.gather(
-        ops_test.model.deploy(
-            f"./{charm}", resources=resources, application_name=APP_NAME, series="jammy"
-        ),
-        ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
-        ),
+    arrange: deploy the cloudflare-configurator charm with related charms.
+    act: relate the cloudflare-configurator charm with related charms.
+    assert: no error happens.
+    """
+    await model.integrate(f"{cloudflare_configurator.name}:ingress", ingress_requirer.name)
+    await model.integrate(
+        f"{cloudflare_configurator.name}:cloudflared-route", cloudflared_route_requirer.name
     )
+    await model.wait_for_idle(
+        apps=[
+            cloudflare_configurator.name,
+            ingress_requirer.name,
+            cloudflared_route_requirer.name,
+        ],
+    )
+
+
+async def test_set_tunnel_token(model, cloudflare_configurator, cloudflared_route_requirer):
+    """
+    arrange: deploy the cloudflare-configurator charm with related charms.
+    act: set tunnel-token charm configuration of the cloudflare-configurator charm.
+    assert: cloudflare-configurator charm pass the tunnel-token to cloudflared-route requirers.
+    """
+    secret_id = await model.add_secret(name="tunnel-token", data_args=["tunnel-token=foobar"])
+    secret_id = secret_id.strip()
+    await model.grant_secret("tunnel-token", cloudflare_configurator.name)
+    await cloudflare_configurator.set_config({"domain": "example.com", "tunnel-token": secret_id})
+    await model.wait_for_idle(status="active")
+    action = await cloudflared_route_requirer.units[0].run_action(
+        "rpc", method="get_tunnel_tokens"
+    )
+    await action.wait()
+    assert json.loads(action.results["return"]) == ["foobar"]
+
+
+async def test_get_ingress_data(model, cloudflare_configurator, ingress_requirer):
+    """
+    arrange: deploy the cloudflare-configurator charm with related charms.
+    act: run get-ingress-data charm action.
+    assert: get-ingress-data charm action dumps ingress integration data.
+    """
+    action = await cloudflare_configurator.units[0].run_action("get-ingress-data")
+    await action.wait()
+    ingress_data = json.loads(action.results["ingress"])
+    assert ingress_data["application-data"]["model"] == model.name
+    assert ingress_data["application-data"]["name"] == ingress_requirer.name
+    assert len(ingress_data["unit-data"]) == len(ingress_requirer.units)
